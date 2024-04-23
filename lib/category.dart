2a2a84bc28f85ec3935ae5ea4f1fb5dbeb50c398
webeights <elatorre.freelance@gmail.com>
2024-04-23 11:05:27 +0800

    added category

---
+++ lib/category.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Category extends StatefulWidget {
+  const Category({super.key});
+
+  @override
+  State<Category> createState() => _CategoryState();
+}
+
+class _CategoryState extends State<Category> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('erish pogi'),
+      ),
+    );
+  }
+}
